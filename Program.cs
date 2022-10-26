fec4953b9e56e55fffab6dcb4f40db04c38f96be
RM <RM@DESKTOP-5H9VH9L>
2022-10-26 17:15:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Stash
+{
+    internal class Stash1
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * EVEN NUMBERS
+             int sum = 0;
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int input3 = Convert.ToInt32(Console.ReadLine());
+
+            if (input1 % 2 == 0)
+            {
+                sum = sum + input1;
+            }
+            if (input2 % 2 == 0)
+            {
+                sum = sum + input2;
+            }
+            if (input3 % 2 == 0)
+            {
+                sum = sum + input3;
+            }
+
+            Console.WriteLine(sum);
+             */
+
+
+        }
+    }
+}
